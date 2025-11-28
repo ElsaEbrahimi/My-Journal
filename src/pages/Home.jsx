4b4d652bc6caa0df5f1aaab4552718ca4ed1662c
Elsa Ebrahimi <teaserpluss@gmail.com>
2025-11-28 15:05:28 +0330

    add Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+const Home = () => {
+  return (
+    <>
+      <div className="hero min-h-screen bg-[url('bg.jpg')]">
+        <div className="hero-overlay"></div>
+        <div className="hero-content text-neutral-content text-center">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-8xl font-bold">My Journal</h1>
+            <p className="mb-5 text-2xl">
+              Put your thoughts on the page and let your mind breathe
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Home;
