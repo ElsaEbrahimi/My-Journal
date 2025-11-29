b2e3e385fa72031073d34dfc4d47e55fff92038f
Elsa Ebrahimi <teaserpluss@gmail.com>
2025-11-29 13:08:24 +0330

    edit Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import Home from "./pages/Home";
 import MainLayout from "./layout/MainLayout";
 import { Route, Routes } from "react-router";
+import Journals from "./pages/Journals";
+import AddJournal from "./components/AddJournal";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
-
-          {/* <Route path="journals" element={<Journals />} />
-          <Route path="*" element={<NotFound />} /> */}
+          <Route path="journals" element={<Journals />} />
+          <Route path="addjournal" element={<AddJournal />} />
+          {/* <Route path="*" element={<NotFound />} /> */}
         </Route>
       </Routes>
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,33 @@
 const Home = () => {
   return (
     <>
-      <div className="hero min-h-screen bg-[url('bg.jpg')]">
-        <div className="hero-overlay"></div>
-        <div className="hero-content text-neutral-content text-center">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-8xl font-bold">My Journal</h1>
-            <p className="mb-5 text-2xl">
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img src="bg.png" className="max-w-xl rounded-lg" />
+          <div>
+            <h1 className="text-5xl font-bold text-(--primary-color)">
+              My Journal
+            </h1>
+            <p className="py-6">
               Put your thoughts on the page and let your mind breathe
             </p>
+            <button className="btn btn-primary bg-(--primary-color)">
+              See More
+            </button>
           </div>
         </div>
       </div>
+      {/* <div className="hero min-h-screen bg-[url('bg.jpg')]">
+        <div className="hero-overlay"></div>
+        <div className="hero-content text-neutral-content text-center">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-8xl font-bold"></h1>
+            <p className="mb-5 text-2xl">
+              
+            </p>
+          </div>
+        </div>
+      </div> */}
     </>
   );
 };
